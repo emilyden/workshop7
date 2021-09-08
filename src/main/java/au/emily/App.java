421af0273f5ad446a8f36f7b90a60743389226b7
emilyden <emkokeefe@gmail.com>
2021-09-08 14:57:40 +1000

    Java PRB App Demo

---
+++ src/main/java/au/emily/App.java
@@ -0,0 +1,16 @@
+package au.emily;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public static boolean isDivisibleByFive(int number){
+        return number % 5 == 0;
+        }
+}
